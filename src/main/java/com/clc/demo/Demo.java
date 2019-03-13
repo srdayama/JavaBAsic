f5948e327893b850ed6cba8af2b458a5a009c73e
SHEETAL <SHEETAL@SHEETAL-PC>
2019-03-13 11:28:18 +0530

    added m1 method in demo

--- src/main/java/com/clc/demo/Demo.java
+++ src/main/java/com/clc/demo/Demo.java
@@ -2,8 +2,13 @@ package com.clc.demo;
 
 public class Demo {
 
+public static void m1(){
+		int a=10;
+		System.out.println("m1 method");
+	}
 	public static void main(String[] args) {
 		System.out.println("hiiiiiiiii");
+		
 	}
 
 }
