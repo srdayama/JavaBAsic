b6ebf22d607e21114c3ab1e39c9b90df32664d62
sharad bhamkar <sharad bhamkar@sharad>
2019-03-13 11:27:53 +0530

    method is created

--- src/main/java/com/clc/demo/Demo.java
+++ src/main/java/com/clc/demo/Demo.java
@@ -2,8 +2,12 @@ package com.clc.demo;
 
 public class Demo {
 
+	public static void m1() {
+		System.out.println("method is created");
+	}
 	public static void main(String[] args) {
 		System.out.println("hiiiiiiiii");
+		System.out.println("");
 	}
 
 }
