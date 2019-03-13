30d2e646b692817f66a259ccc476f91254e514fd
sharad bhamkar <sharad bhamkar@sharad>
2019-03-13 15:14:33 +0530

    demo 4 is updated

--- src/main/java/com/clc/demo/Demo4.java
+++ src/main/java/com/clc/demo/Demo4.java
@@ -4,6 +4,9 @@ public class Demo4 {
 
 	public static void main(String[] args) {
 			System.out.println("demo class 4 11111111111111111111 ");
+			int i=10;
+			System.out.println("integer i:"+i);
+			
 	}
 
 }
