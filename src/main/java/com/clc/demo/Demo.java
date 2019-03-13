ac83c4a8f65b9239e5976365f153ca864a49caeb
SHEETAL <SHEETAL@SHEETAL-PC>
2019-03-13 11:45:31 +0530

    commited m2

--- src/main/java/com/clc/demo/Demo.java
+++ src/main/java/com/clc/demo/Demo.java
@@ -5,6 +5,11 @@ public class Demo {
 	public static void m1() {
 		System.out.println("method is created");
 	}
+	public static void m2() {
+		System.out.println("method  2 is created");
+	}
+
+	
 	public static void main(String[] args) {
 		System.out.println("hiiiiiiiii");
 		System.out.println("");
