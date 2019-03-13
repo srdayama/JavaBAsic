9a7ee1a62b2c037381fc20d8dcc6f10638878d87
SHEETAL <SHEETAL@SHEETAL-PC>
2019-03-13 15:17:06 +0530

    Demo4 is updated by sheetal

--- src/main/java/com/clc/demo/Demo4.java
+++ src/main/java/com/clc/demo/Demo4.java
@@ -2,6 +2,9 @@ package com.clc.demo;
 
 public class Demo4 {
 
+	static void method1(){
+		System.out.println("Method1 ....");
+	}
 	public static void main(String[] args) {
 			System.out.println("demo class 4 11111111111111111111 ");
 			int i=10;
