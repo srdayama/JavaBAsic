2dc2f948ce110dfbc3b8e53841abac88c2f0a1a2
sharad bhamkar <sharad bhamkar@sharad>
2019-03-13 13:27:43 +0530

    demo 4 change

--- src/main/java/com/clc/demo/Demo4.java
+++ src/main/java/com/clc/demo/Demo4.java
@@ -3,7 +3,7 @@ package com.clc.demo;
 public class Demo4 {
 
 	public static void main(String[] args) {
-			System.out.println("demo class 4 ");
+			System.out.println("demo class 4 11111111111111111111 ");
 	}
 
 }
