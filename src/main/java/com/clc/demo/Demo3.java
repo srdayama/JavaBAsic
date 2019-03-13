f70189d204b99fc25291c30b22d18a52d19fadb4
SHEETAL <SHEETAL@SHEETAL-PC>
2019-03-13 15:34:48 +0530

    Demo3 updated

--- src/main/java/com/clc/demo/Demo3.java
+++ src/main/java/com/clc/demo/Demo3.java
@@ -3,7 +3,9 @@ package com.clc.demo;
 public class Demo3 {
 
 	public static void main(String[] args) {
-System.out.println("Demo3");
+    System.out.println("Demo3");
+    System.out.println("Demo3 is updating");
+
 	}
 
 }
