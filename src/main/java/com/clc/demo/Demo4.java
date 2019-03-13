97e5c8c63894ad02416785c45e7028dad5b864b5
SHEETAL <SHEETAL@SHEETAL-PC>
2019-03-13 15:26:56 +0530

    Method2 added

--- src/main/java/com/clc/demo/Demo4.java
+++ src/main/java/com/clc/demo/Demo4.java
@@ -5,6 +5,10 @@ public class Demo4 {
 	static void method1(){
 		System.out.println("Method1 ....");
 	}
+	
+	static void method2(){
+		System.out.println("Method2........ ....");
+	}
 	public static void main(String[] args) {
 			System.out.println("demo class 4 11111111111111111111 ");
 			int i=10;
@@ -12,6 +16,7 @@ public class Demo4 {
 			System.out.println("updated");
 			System.out.println("inserted");
 			System.out.println("aaaaaaaaaaaaaaaaaaaaaaa");
+			
 			
 	}
 
